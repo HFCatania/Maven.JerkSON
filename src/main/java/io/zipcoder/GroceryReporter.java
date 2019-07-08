251ebb1c781c347f35b5c5f0381d358d50200fef
Henry <cataniahf@gmail.com>
2019-07-08 16:45:00 -0400

    Not even kinda

--- src/main/java/io/zipcoder/GroceryReporter.java
+++ src/main/java/io/zipcoder/GroceryReporter.java
@@ -1,12 +1,77 @@
 package io.zipcoder;
 
+import com.sun.xml.internal.xsom.impl.scd.Iterators;
 import io.zipcoder.utils.FileReader;
+import io.zipcoder.utils.match.Match;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class GroceryReporter {
     private final String originalFileText;
 
     public GroceryReporter(String jerksonFileName) {
         this.originalFileText = FileReader.readFile(jerksonFileName);
+    }
+
+    public List<String> inputNormalizer(){
+        try {
+            Path path = Paths.get(FileReader.readFile(getOriginalFileText()));
+            Stream<String> lines = Files.lines(path);
+            lines.map(line -> line.replaceAll("@", ":"));
+            lines.map(line -> line.replaceAll("\\^", ":"));
+            lines.map(line -> line.replaceAll("\\*", ":"));
+            lines.map(line -> line.replaceAll("%", ":"));
+            List<String> normalized = lines.collect(Collectors.toList());
+
+            return normalized;
+
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        return null;
+        }
+
+    public List<String> inputSeperator(){
+            String normalizedString = inputNormalizer().toString();
+            Pattern pattern = Pattern.compile("\\S+\\s*##\\s*\\S+");
+            Matcher matcher = pattern.matcher(normalizedString);
+            List<String> splitString = new ArrayList<>();
+            while(matcher.find()){
+                String subStr = matcher.group(1);
+                splitString.add(subStr);
+            }
+
+        return splitString;
+
+    }
+
+
+
+    public HashMap<String, Double> mapMap() {
+        Map<String, Double> itemMap = new HashMap<>();
+        List<String> listOne = inputSeperator();
+        List<String> listTwo = inputSeperator(); 
+        return null;
+    }
+
+    public String stringify(){
+       return inputSeperator().toString();
+    }
+
+
+
+
+
+    public String getOriginalFileText() {
+        return originalFileText;
     }
 
     @Override
--- src/main/java/io/zipcoder/ItemParser.java
+++ src/main/java/io/zipcoder/ItemParser.java
@@ -3,11 +3,22 @@ package io.zipcoder;
 import io.zipcoder.utils.Item;
 import io.zipcoder.utils.ItemParseException;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ItemParser {
     public List<Item> parseItemList(String valueToParse) {
-        return null;
+        Pattern pattern = Pattern.compile("\\S+\\s*##\\s*\\S+");
+        Matcher matcher = pattern.matcher(valueToParse);
+        List<Item> splitString = new ArrayList<>();
+        while(matcher.find()){
+            Item item = matcher.group(1);
+            splitString.add(item);
+        }
+
+        return splitString;
     }
 
     public Item parseSingleItem(String singleItem) throws ItemParseException {
